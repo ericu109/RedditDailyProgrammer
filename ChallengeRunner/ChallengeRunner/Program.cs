06d50a80c3b02bdcd6ffc004a81b6cac5306f890
ericu109 <urbina.eric@gmail.com>
2015-10-05 19:46:42 -0500

    Updated the console app to handle running a specific challenge instead of just the most recent.

--- ChallengeRunner/ChallengeRunner/Program.cs
+++ ChallengeRunner/ChallengeRunner/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,17 +14,43 @@ namespace ChallengeRunner
    {
       static void Main(string[] args)
       {
-         ChallengeBase c = mostRecentChallenge();//Get the most recent challenge file
-         c.run();//run the challenge
-         Console.WriteLine(c.passed? "Challenge Passed" : "Challenge Failed");//Output weather or not the challenge was passed.
+         ChallengeBase challengeToExecute = getChallenge();
+         challengeToExecute.run();//run the challenge
+         Console.WriteLine(challengeToExecute.passed? "Challenge Passed" : "Challenge Failed");//Output weather or not the challenge was passed.
          Console.ReadLine();//Pause
+      }
+
+      private static ChallengeBase getChallenge(string challenge = "")
+      {
+         if (string.IsNullOrEmpty(challenge))
+         {
+            return mostRecentChallenge();
+         }
+         else
+         {
+            return getChallengeWithName(challenge);
+         }
+      }
+
+      private static ChallengeBase getChallengeWithName(string challengeName)
+      {
+         if (string.IsNullOrEmpty(challengeName))
+         {
+            throw new ArgumentNullException(nameof(challengeName));
+         }
+
+         var challenge = Assembly.GetExecutingAssembly().GetTypes().FirstOrDefault(m => m.Name == challengeName);
+
+         if (challenge != null)
+         {
+            return (ChallengeBase) Activator.CreateInstance(challenge);
+         }
+         throw new Exception($"Could not find challenge {challengeName} to run");
       }
 
       private static ChallengeBase mostRecentChallenge()
       {
-         var assembly = Assembly.GetExecutingAssembly();
-
-         var challenges = assembly.GetTypes().Where(m => m.Name.Contains("Challenge") && m.Name != "ChallengeBase");
+         var challenges = Assembly.GetExecutingAssembly().GetTypes().Where(m => m.Name.Contains("Challenge") && m.Name != "ChallengeBase");
 
          Type mostRecentChallenge = null;
          int highestChallengeNumber = 0;
